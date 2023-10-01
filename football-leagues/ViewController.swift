6b9b048b66ae670aa6d82208718f589a50ba5def
Khaled Mohsen <mohsenkhaled803@gmail.com>
2023-10-01 10:04:15 +0300

    Initial Commit

---
+++ football-leagues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  football-leagues
+//
+//  Created by Innovitics on 01/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
