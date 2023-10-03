e2773fb6ce7f69f1a3a5698017f9af1063df2768
Khaled Mohsen <mohsenkhaled803@gmail.com>
2023-10-03 16:56:21 +0300

    create competitionsTableViewCell design

---
+++ football-leagues/View/competitionsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  competitionsTableViewCell.swift
+//  football-leagues
+//
+//  Created by Innovitics on 03/10/2023.
+//
+
+import UIKit
+
+class competitionsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
