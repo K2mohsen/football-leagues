c91fe5f4221b4eec4bb0e35f59558fb10d6a9ada
Khaled Mohsen <mohsenkhaled803@gmail.com>
2023-10-15 15:45:57 +0300

    register tableView and reusable cell with identifier and make error alert

--- football-leagues/View/CompetitionsViewController.swift
+++ football-leagues/View/CompetitionsViewController.swift
@@ -1,9 +1,3 @@
-//
-//  CompetitionsViewController.swift
-//  football-leagues
-//
-//  Created by Innovitics on 01/10/2023.
-//
 
 import UIKit
 
@@ -13,12 +7,45 @@ class CompetitionsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UINib.init(nibName: "competitionsCell", bundle: nil), forCellReuseIdentifier: "competitionsCell")
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         competitionsVM.successClouser = {
-            // relode data 
+            self.tableView.reloadData()
         }
         
         competitionsVM.errorClouser = { error in
+            self.showError(error)
         }
+        competitionsVM.getcompetitions()
     }
+    func showError(_ errorMessage : String){
+        let alertController = UIAlertController(title: "error", message: errorMessage, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(action)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+}
+//MARK: - tableViewDataSource
+extension CompetitionsViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        competitionsVM.competitions.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "competitionsCell", for: indexPath) as! CompetitionsTableViewCell
+        let competition = competitionsVM.competitions[indexPath.row]
+        return cell
+    }
+}
+//MARK: - tableViewDelegate
+extension CompetitionsViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let competitionId = competitionsVM.competitions[indexPath.row].id
+    }
+    
+    
 }
 
