c8b7f9e8dd57c7b1412f1239a3f84c2ecdd59ef7
Khaled Mohsen <mohsenkhaled803@gmail.com>
2023-10-15 15:44:37 +0300

    rename CompetiotionsTableViewCell with capital "C"

--- football-leagues/View/competitionsTableViewCell.swift
+++ football-leagues/View/competitionsTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class competitionsTableViewCell: UITableViewCell {
+class CompetitionsTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
