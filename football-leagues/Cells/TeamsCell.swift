7bd5adb194d767052edb80858fc29b6f533aaf62
Khaled Mohsen <mohsenkhaled803@gmail.com>
2023-10-17 15:22:59 +0300

    create TeamsCell

---
+++ football-leagues/Cells/TeamsCell.swift
@@ -0,0 +1,23 @@
+//
+//  TeamsCell.swift
+//  football-leagues
+//
+//  Created by Innovitics on 17/10/2023.
+//
+
+import UIKit
+
+class TeamsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
