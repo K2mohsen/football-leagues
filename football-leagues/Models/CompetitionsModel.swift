8a43a37f2e24655fbb5ee46780d2f9e8ad69aa99
Khaled Mohsen <mohsenkhaled803@gmail.com>
2023-10-17 13:02:45 +0300

    update CompetionsModel

--- football-leagues/Models/CompetitionsModel.swift
+++ football-leagues/Models/CompetitionsModel.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 // MARK: - Welcome
-struct Welcome: Codable {
+struct Competitions: Codable {
     let count: Int?
     let filters: Filters?
     let competitions: [Competition]?
@@ -20,7 +20,18 @@ struct Competition: Codable {
     let numberOfAvailableSeasons: Int?
     let lastUpdated: String?
 }
-
+enum Code: String, Codable {
+    case ecq = "ECQ"
+    case qafc = "QAFC"
+    case qcbl = "QCBL"
+    case wc = "WC"
+}
+enum Name: String, Codable {
+    case europeanChampionshipQualifiers = "European Championship Qualifiers"
+    case fifaWorldCup = "FIFA World Cup"
+    case wcQualificationAFC = "WC Qualification AFC"
+    case wcQualificationCONMEBOL = "WC Qualification CONMEBOL"
+}
 // MARK: - Area
 struct Area: Codable {
     let id: Int?
@@ -43,7 +54,6 @@ enum TypeEnum: String, Codable {
     case cup = "CUP"
     case league = "LEAGUE"
 }
-
 // MARK: - Filters
 struct Filters: Codable {
     let client: String?
