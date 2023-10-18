417fb5ac5784456a513fb0e35ea25030a58064d3
Khaled Mohsen <mohsenkhaled803@gmail.com>
2023-10-18 13:48:52 +0300

    update NetworkManager and create TeamsVM

--- football-leagues/Networking/NetworkManager.swift
+++ football-leagues/Networking/NetworkManager.swift
@@ -2,7 +2,11 @@
 import Foundation
 import Alamofire
 
-
+let compURL = "https://api.football-data.org/v4/competitions"
+let teamsURL = "http://api.football-data.org/v4/competitions/WC/teams"
+let apiKey = "7e21461aac484b229189d31e2abad0f1"
+let headers : HTTPHeaders = [ "X-Auth-Token": apiKey]
+   
 class APIService{
     // singletone property
     static let shared = APIService()
@@ -10,19 +14,24 @@ class APIService{
     private init () {}
     
     func fetchCompetitions(completion: @escaping ([Competition]?, String?) -> Void) {
-        let baseURL = "https://api.football-data.org/v4/competitions"
-        let apiKey = "7e21461aac484b229189d31e2abad0f1"
-        let url = baseURL
-        let headers : HTTPHeaders = [
-            "X-Auth-Token": apiKey
-        ]
-        let request = AF.request(url, method: .get, headers: headers)
+        let request = AF.request(compURL, method: .get, headers: headers)
         request.responseDecodable(of: Competitions.self) { response in
             switch response.result {
             case .success(let response):
                 completion(response.competitions, nil)
             case .failure(let error) :
                // print("error fetching data :\(error)")
+                completion(nil, error.errorDescription)
+        }
+    }
+}
+    func fetchTeams(completion : @escaping ([Team]?, String?) -> Void) {
+        let request = AF.request(teamsURL , method: .get, headers: headers)
+        request.responseDecodable(of:Teams.self) { response in
+            switch response.result {
+            case .success(let response):
+                completion(response.teams, nil)
+            case .failure(let error) :
                 completion(nil, error.errorDescription)
             }
         }
---
+++ football-leagues/ViewModels/TeamsVM.swift
@@ -0,0 +1,8 @@
+//
+//  TeamsVM.swift
+//  football-leagues
+//
+//  Created by Innovitics on 18/10/2023.
+//
+
+import Foundation
