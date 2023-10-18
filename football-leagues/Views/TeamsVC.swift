fbaaeaab3e1b8668fa4d6e353a453e7ab78916af
Khaled Mohsen <mohsenkhaled803@gmail.com>
2023-10-18 12:10:33 +0300

    Create TeamsVC and it's xib file

---
+++ football-leagues/Views/TeamsVC.swift
@@ -0,0 +1,17 @@
+//
+//  TeamsVC.swift
+//  football-leagues
+//
+//  Created by Innovitics on 18/10/2023.
+//
+
+import UIKit
+
+class TeamsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
